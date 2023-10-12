a2baa8a84eb084e3041094e8aea1bcba3fd386d0
Mapopo (Evans) <mppdgreat@gmail.com>
2023-10-12 02:22:39 +0100

    Show Hello ___ message on press button

--- screens/Login.js
+++ screens/Login.js
@@ -56,23 +56,25 @@ const Login = ({loggedInState, loggedInStates,setLoggedInState})=>{
             <TouchableOpacity
                style={styles.sendButton}
               onPress={async ()=>{
-                console.log(phoneNumber+' Button was pressed')
+                // console.log(phoneNumber+' Button was pressed')
+                console.log("Hello Evans")
+                Alert.alert("Hello Evans");
     
-                const sendTextResponse=await fetch(
-                  'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
-                  {
-                    method:'POST',
-                    headers:{
-                     'content-type':'application/text'
-                   }
-                  }
-                )
-                const sendTextResponseData = await sendTextResponse.text();
-                if(sendTextResponse.status!=200){//invalid phone number, send them to the signup page
-                  await Alert.alert("Did you type your number correctly? "+phoneNumber);
-                } else{
-                  setLoggedInState(loggedInStates.LOGGING_IN);
-                }
+                // const sendTextResponse=await fetch(
+                //   'https://dev.stedi.me/twofactorlogin/'+phoneNumber,
+                //   {
+                //     method:'POST',
+                //     headers:{
+                //      'content-type':'application/text'
+                //    }
+                //   }
+                // )
+                // const sendTextResponseData = await sendTextResponse.text();
+                // if(sendTextResponse.status!=200){//invalid phone number, send them to the signup page
+                //   await Alert.alert("Did you type your number correctly? "+phoneNumber);
+                // } else{
+                //   setLoggedInState(loggedInStates.LOGGING_IN);
+                // }
               }}
             >
               <Text style={{color:'white'}}>Send</Text>      
